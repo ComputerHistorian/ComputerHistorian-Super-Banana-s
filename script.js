d572ecdcc8c80d2232f26133f1f05b39e8ee1754
ComputerHistorian <corazone2014@gmail.com>
2026-01-21 07:54:25 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const hamburger = document.getElementById("hamburger");
+    const sideCard = document.getElementById("sideCard");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      sideCard.classList.toggle("show");
+    });
+    function sigmo() {
+  const bodyElement = document.getElementById("parent");
+
+  if (!bodyElement.querySelector("#batDuck")) {
+    const img = document.createElement("img");
+    img.src = "https://mimo.app/i/batduck.png";
+    img.id = "batDuck"; // 💥 помечаем уточку
+    bodyElement.appendChild(img);
+
+    const msg = document.createElement("p");
+    msg.id = "duckMsg"; // 💥 помечаем сообщение
+    msg.textContent = "💀";
+    msg.style.fontSize = "15px";
+    bodyElement.appendChild(msg);
+  } else {
+    const msg = document.getElementById("duckMsg");
+    if (msg) msg.textContent = "☠️🦆";
+  }
+}
+function siggg() {
+  // ищем картинку уточки по id
+  const img = document.getElementById("batDuck");
+  const msg = document.getElementById("duckMsg");
+
+  if (img) img.remove(); // удаляем картинку
+  if (msg) msg.remove(); // удаляем сообщение
+} 
